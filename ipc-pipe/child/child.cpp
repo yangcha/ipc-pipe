a576164a690780b7f5314568f9b08da87093f69c
Changjiang Yang <changjiangyang@noahmed.com>
2024-03-13 18:21:44 -0400

    set cin to binary mode

--- ipc-pipe/child/child.cpp
+++ ipc-pipe/child/child.cpp
@@ -3,11 +3,22 @@
 
 #include <iostream>
 #include <thread>
+#include <io.h>
+#include <fcntl.h>
 
 using namespace std::chrono_literals;
 
 int main()
 {
+	// Set "stdin" to have binary mode:
+	int result = _setmode(_fileno(stdin), _O_BINARY);
+	if (result == -1) {
+		std::cerr << "Cannot set mode\n";
+	}
+	else {
+		std::cout << "'stdin' successfully changed to binary mode\n";
+	}
+
 	for (int i = 0; ; i++)
 	{
 		std::cerr << i << " seconds since epoch\n";
